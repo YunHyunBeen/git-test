d41c2755a6235dc85961a047a08cb24752990542
YunHyunBeen <nsntks19@naver.com>
2020-11-01 17:11:31 +0900

    -

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React tttt
+          Learn React tttt22
         </a>
       </header>
     </div>
